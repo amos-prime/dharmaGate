613e83810463198852b9288c9897fd4fa2822013
Artur Mos <artur.mos@gmail.com>
2018-08-30 20:40:35 +0200

    Basic Project Dependencies, embedded MongoDB

---
+++ src/main/java/pl/zazen/dharmagate/DharmagateApplication.java
@@ -0,0 +1,13 @@
+package pl.zazen.dharmagate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class DharmagateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DharmagateApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/zazen/dharmagate/DharmagateApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.zazen.dharmagate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DharmagateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
